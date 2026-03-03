ba1a4a12f24364d1f2d04e14c3d98fea17ea4636
Gabriel da Silva Cruz <80924942+oGabrielCruz@users.noreply.github.com>
2026-03-03 19:26:27 +0000

    feat: add folder tree functionality and status API enhancements
    - Introduced a new folder tree structure in the documentation with interactive features using vanilla JavaScript.
    - Implemented a sidebar toggle for better navigation within the documentation.
    - Added CSS styles for the folder tree and sidebar for improved UI/UX.
    - Enhanced the status API to return detailed database information including version, max connections, and currently opened connections.
    - Updated integration tests to validate the new status API response structure and values.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,14 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  try {
-    const result = await database.query("SELECT 1 + 1 as sum;");
-    console.log(result.rows);
-    response.status(200).json({ status: "ok", sum: result.rows });
-  } catch (err) {
-    console.error("/api/v1/status error", err);
-    response.status(200).json({ error: "database query failed" });
-  }
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*) FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        open_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
