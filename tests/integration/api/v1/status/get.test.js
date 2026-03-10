b090cb82805b6cc3debfd79b500edd8600ac3f9a
Gabriel da Silva Cruz <80924942+oGabrielCruz@users.noreply.github.com>
2026-03-10 14:56:53 +0000

    finish `/api/v1/status`endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,10 +1,8 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  console.log(response.body);
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); // Check if it's a valid date
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
