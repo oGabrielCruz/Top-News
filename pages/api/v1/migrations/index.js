e396d6ec8c6ab24f212b740d55ea43c3abf38278
Gabriel da Silva Cruz <80924942+oGabrielCruz@users.noreply.github.com>
2026-04-24 15:04:58 +0000

    feat: merge

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import { resolve } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
@@ -9,24 +9,29 @@ export default async function migrations(request, response) {
       error: `Method "${request.method}" not allowed`,
     });
   }
+
   let dbClient;
+
   try {
     dbClient = await database.getNewClient();
-    const defaultMigrationsOptions = {
+
+    const defaultMigrationOptions = {
       dbClient: dbClient,
       dryRun: true,
-      dir: join("infra", "migrations"),
+      dir: resolve("infra", "migrations"),
       direction: "up",
-      verbose: "true",
+      verbose: true,
       migrationsTable: "pgmigrations",
     };
+
     if (request.method === "GET") {
-      const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-      response.status(200).json(pendingMigrations);
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      return response.status(200).json(pendingMigrations);
     }
+
     if (request.method === "POST") {
       const migratedMigrations = await migrationRunner({
-        ...defaultMigrationsOptions,
+        ...defaultMigrationOptions,
         dryRun: false,
       });
 
